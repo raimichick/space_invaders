09d45e5d9a7de8c0f7622b5d4c607e2b862d674c
Craig Harris <70126287+cxhx441@users.noreply.github.com>
2024-04-13 12:20:59 -0700

    repo setup

---
+++ include/foo.h
@@ -0,0 +1,8 @@
+//
+// Created by cxhx4 on 4/13/2024.
+//
+
+#ifndef FOO_H
+#define FOO_H
+
+#endif //FOO_H
---
+++ src/main.c
@@ -0,0 +1,9 @@
+//
+// Created by cxhx4 on 4/13/2024.
+//
+#include <stdio.h>
+
+int main(){
+    printf("Hello World!\n");
+    return 0;
+}
