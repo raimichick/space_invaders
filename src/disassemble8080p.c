8ff7058e2137120662ba7af977df9d9c46090b2b
Craig Harris <70126287+cxhx441@users.noreply.github.com>
2024-04-13 14:59:20 -0700

    disassembler file template. Switch case to be updated to handle each opcode:instruction.

---
+++ src/disassemble8080p.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int disassemble8080p(unsigned char *codebuffer, int pc)
+{
+    /*
+    TODO: Base code copied from emulator 101 website: CHANGE THIS AROUND TO MAKE IT OUR OWN!
+    TODO: Add opcode-to-instruction cases.
+    */
+    unsigned char *code = &codebuffer[pc];
+    int opbytes = 1;
+    printf("%04x ", pc);
+    switch (*code)
+    {
+        // clang-format off
+        case 0x00: printf("NOP");  break;
+        default: printf("NOT IMPLEMENTED NOT IMPLEMENTED NOT IMPLEMENTED NOT IMPLEMENTED");
+        // clang-format on
+    }
+    printf("\n");
+    return opbytes;
+}
