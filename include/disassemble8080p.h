3d9abf2308f8b4d3f4878f443b6498e8730d578a
Damian <mystelaurora@gmail.com>
2024-04-16 21:15:51 -0400

    Display disassembled code

---
+++ include/disassemble8080p.h
@@ -0,0 +1,10 @@
+//
+// Created by Ayu on 4/16/2024.
+//
+
+#ifndef DISASSEMBLE8080P_H
+#define DISASSEMBLE8080P_H
+
+int disassemble8080p(unsigned char *codebuffer, int pc);
+
+#endif // DISASSEMBLE8080P_H
--- src/main.c
+++ src/main.c
@@ -1,12 +1,37 @@
 //
 // Created by cxhx4 on 4/13/2024.
 //
+#include "../include/disassemble8080p.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    printf("Hello World!\n");
-    printf("Second Hello!\n");
-    printf("Third Hello\n");
-    return 0;
+    // Open the game file
+    FILE *invaders_file = fopen("../include/invaders_combined", "rb");
+    if (invaders_file == NULL)
+    {
+        perror("Error: Could not open invaders_combined");
+        exit(1);
+    }
+
+    // Allocate space for the game data
+    fseek(invaders_file, 0L, SEEK_END);
+    const int invaders_size = ftell(invaders_file);
+    fseek(invaders_file, 0L, SEEK_SET);
+    unsigned char *invaders_game = malloc(invaders_size);
+
+    // Copy file into allocated space
+    fread(invaders_game, invaders_size, 1, invaders_file);
+
+    // Close the file
+    fclose(invaders_file);
+
+    // Print disassembly
+    int dis_i = 0;
+    while (dis_i < invaders_size)
+    {
+        int opbytes = disassemble8080p(invaders_game, dis_i);
+        dis_i += opbytes;
+    }
 }
