f4b339283ca61e72762bd3e106c3f3191bcbe2fd
Craig H <70126287+cxhx441@users.noreply.github.com>
2024-04-20 09:58:17 -0700

    move struct to their own header

---
+++ include/structs.h
@@ -0,0 +1,27 @@
+#include <stdint.h>
+
+typedef struct Conditions
+{
+    uint8_t zero : 1;
+    uint8_t sign : 1;
+    uint8_t parity : 1;
+    uint8_t carry : 1;
+    uint8_t aux_carry : 1;
+    uint8_t pad : 3;
+} Conditions;
+
+typedef struct State
+{
+    uint8_t a;
+    uint8_t b;
+    uint8_t c;
+    uint8_t d;
+    uint8_t e;
+    uint8_t h;
+    uint8_t l;
+    uint16_t stack_pointer;
+    uint16_t program_counter;
+    uint8_t *memory;
+    Conditions conditions;
+    uint8_t int_enable;
+} State;
