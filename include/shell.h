00937d6426ba3f5e3aa7e236480ff3977155ff67
Craig H <70126287+cxhx441@users.noreply.github.com>
2024-04-21 00:01:51 -0700

    reformat

--- include/shell.h
+++ include/shell.h
@@ -11,7 +11,8 @@ void push_register_pair_to_stack(struct State *state, uint8_t rh, uint8_t rl);
 void pop_stack_to_program_counter(struct State *state);
 void pop_stack_to_register_pair(struct State *state, uint8_t *rh, uint8_t *rl);
 void mov_reg_to_reg(struct State *state, uint8_t *to, uint8_t *from);
-void subtract_instruction(struct State* state, uint8_t minuend, uint8_t subtrahend);
+void subtract_instruction(struct State *state, uint8_t minuend,
+                          uint8_t subtrahend);
 void wait_cycles(int clockCycles);
 uint8_t get_sign_flag(uint8_t register_value);
 uint8_t get_zero_flag(uint8_t register_value);
