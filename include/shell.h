69d9ea40f7a9194b9d2af0e589fd102d8d6cbacf
hermnich <122328256+hermnich@users.noreply.github.com>
2024-04-22 14:36:59 -0400

    Finishing up the commands for the first 50 0x00-0x1a, Added a helper function to combine two registers into a single 16 bit number

--- include/shell.h
+++ include/shell.h
@@ -14,8 +14,10 @@ void mov_reg_to_reg(struct State *state, uint8_t *to, uint8_t *from);
 void subtract_instruction(struct State *state, uint8_t minuend,
                           uint8_t subtrahend);
 void wait_cycles(int clockCycles);
+uint16_t combine_registers(uint8_t reg1, uint8_t reg2);
 uint8_t get_sign_flag(uint8_t register_value);
 uint8_t get_zero_flag(uint8_t register_value);
 uint8_t get_parity_flag(uint8_t register_value);
 uint8_t get_carry_flag_from_sum(uint8_t val0, uint8_t val1);
+uint8_t get_carry_flag_from_sum(uint16_t val0, uint16_t val1);
 uint8_t get_aux_carry_flag_from_sum(uint8_t val0, uint8_t val1);
