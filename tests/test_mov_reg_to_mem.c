5ec577abb6f346efccd5be4d58d16ca133024815
Damian <mystelaurora@gmail.com>
2024-04-22 20:15:40 -0400

    Fixed inaccurate comment in test_mov_reg_to_mem

--- tests/test_mov_reg_to_mem.c
+++ tests/test_mov_reg_to_mem.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
         state->l = 2;
         // Run command to test
         emulate8080(state);
-        // Validate output - Reg A = 5, pc advanced by 1
+        // Validate output - memory[2] = 5, pc advanced by 1
         if ((state->memory[2] == 5) && state->pc == 1) return 0;
     }
     return 1;
