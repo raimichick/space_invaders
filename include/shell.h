5609c51ec1a64d6ef0c5642bc872d6a10013f0a5
raimichick <101148339+raimichick@users.noreply.github.com>
2024-04-28 20:33:09 -0700

    added function declarations for sub and sbb

--- include/shell.h
+++ include/shell.h
@@ -24,5 +24,7 @@ uint8_t get_parity_flag(uint8_t register_value);
 uint8_t get_carry_flag_from_sum_8b(uint8_t val0, uint8_t val1);
 uint8_t get_carry_flag_from_sum_16b(uint16_t val0, uint16_t val1);
 uint8_t get_aux_carry_flag_from_sum(uint8_t val0, uint8_t val1);
+void sbb(uint8_t *a, uint8_t b, flags *f);
+void sub(uint8_t *a, uint8_t b, flags *f);
 
 void unimplementedInstr(uint8_t opcode);
