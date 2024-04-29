6aff49978805a01004fb62a6183d5a834b19dc32
raimichick <101148339+raimichick@users.noreply.github.com>
2024-04-28 20:43:14 -0700

    trying to get function headers to pass checks

--- include/shell.h
+++ include/shell.h
@@ -24,7 +24,7 @@ uint8_t get_parity_flag(uint8_t register_value);
 uint8_t get_carry_flag_from_sum_8b(uint8_t val0, uint8_t val1);
 uint8_t get_carry_flag_from_sum_16b(uint16_t val0, uint16_t val1);
 uint8_t get_aux_carry_flag_from_sum(uint8_t val0, uint8_t val1);
-void sbb(struct State *state, uint8_t b);
-void sub(struct State *state, uint8_t b);
+void sub(uint8_t *a, uint8_t b, Conditions *f);
+void sbb(uint8_t *a, uint8_t b, Conditions *f);
 
 void unimplementedInstr(uint8_t opcode);
