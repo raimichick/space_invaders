ccaf56d3f3e2f5af9ca574bb854a7ca9078d7865
raimichick <101148339+raimichick@users.noreply.github.com>
2024-05-02 08:11:40 -0700

    Update shell.c

--- src/shell.c
+++ src/shell.c
@@ -729,7 +729,7 @@ void emulate8080(State *state)
     {
         uint8_t immediate = state->memory[state->pc + 1];
         state->pc += 2;
-        uint8_t result = subtract_8b(state, state->a, immediate);
+        uint8_t result = subtract_8b(state, &state->a, immediate);
         state->a = result;
         wait_cycles(7);
         break;
@@ -745,7 +745,7 @@ void emulate8080(State *state)
     {
         uint8_t immediate = state->memory[state->pc + 1];
         state->pc += 2;
-        uint8_t result = subtract_8b(state, state->a, immediate + (state->conditions.carry ? 1 : 0));
+        uint8_t result = subtract_8b(state, &state->a, immediate + (state->conditions.carry ? 1 : 0));
         state->a = result;
         wait_cycles(7);
         break;
