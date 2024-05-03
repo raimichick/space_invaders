7fc230dc9c161828776bd848af9f8b97fdb2f08a
Craig Harris <70126287+cxhx441@users.noreply.github.com>
2024-05-03 11:37:54 -0700

    added helpful error message to state_compare.

--- src/state.c
+++ src/state.c
@@ -4,6 +4,8 @@
 #include "../include/state.h"
 
 #define MEM_SIZE 0x10000
+#define RED_TXT_COLOR "\033[0;31m"
+#define DEFAULT_TXT_COLOR "\033[0m"
 
 State *Init8080(void)
 {
@@ -60,24 +62,28 @@ int state_compare(State *state, State *expected_state)
     // If both states are the same, returns 0, else 1
     // Does not check memory
 
-    if (state->a != expected_state->a) return 1;
-    if (state->b != expected_state->b) return 1;
-    if (state->c != expected_state->c) return 1;
-    if (state->d != expected_state->d) return 1;
-    if (state->e != expected_state->e) return 1;
-    if (state->h != expected_state->h) return 1;
-    if (state->l != expected_state->l) return 1;
-    if (state->sp != expected_state->sp) return 1;
-    if (state->pc != expected_state->pc) return 1;
-    if (state->int_enable != expected_state->int_enable) return 1;
+    // clang-format off
+    printf(RED_TXT_COLOR); // anything printed after this will be in red;
+    if (state->a != expected_state->a)      { printf("Err: A\n");   return 1; }
+    if (state->b != expected_state->b)      { printf("Err: B\n");   return 1; }
+    if (state->c != expected_state->c)      { printf("Err: C\n");   return 1; }
+    if (state->d != expected_state->d)      { printf("Err: D\n");   return 1; }
+    if (state->e != expected_state->e)      { printf("Err: E\n");   return 1; }
+    if (state->h != expected_state->h)      { printf("Err: H\n");   return 1; }
+    if (state->l != expected_state->l)      { printf("Err: L\n");   return 1; }
+    if (state->sp != expected_state->sp)    { printf("Err: SP\n");  return 1; }
+    if (state->pc != expected_state->pc)    { printf("Err: PC\n");  return 1; }
 
-    if (state->conditions.zero != expected_state->conditions.zero) return 1;
-    if (state->conditions.sign != expected_state->conditions.sign) return 1;
-    if (state->conditions.parity != expected_state->conditions.parity) return 1;
-    if (state->conditions.carry != expected_state->conditions.carry) return 1;
-    if (state->conditions.aux_carry != expected_state->conditions.aux_carry)
-        return 1;
-    if (state->conditions.pad != expected_state->conditions.pad) return 1;
+    if (state->int_enable != expected_state->int_enable) { printf("Err: int_enable\n"); return 1; }
+
+    if (state->conditions.zero != expected_state->conditions.zero)          { printf("Err: Zero\n");     return 1; }
+    if (state->conditions.sign != expected_state->conditions.sign)          { printf("Err: Sign\n");     return 1; }
+    if (state->conditions.parity != expected_state->conditions.parity)      { printf("Err: Parity\n");   return 1; }
+    if (state->conditions.carry != expected_state->conditions.carry)        { printf("Err: Carry\n");    return 1; }
+    if (state->conditions.aux_carry != expected_state->conditions.aux_carry){ printf("Err: Aux_Carry\n");return 1; }
+    if (state->conditions.pad != expected_state->conditions.pad)            { printf("Err: Pad\n");      return 1; }
+    printf(DEFAULT_TXT_COLOR); // anything printed after this will be in default color
+    // clang-format on
 
     return 0;
 }
