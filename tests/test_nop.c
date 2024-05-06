5d50aa8ae6af6bbcd9d4a9e87d067ecdca5b4e22
hermnich <122328256+hermnich@users.noreply.github.com>
2024-05-06 16:34:00 -0400

    Test NOP

---
+++ tests/test_nop.c
@@ -0,0 +1,40 @@
+#include "../include/opcodes.h"
+#include "../include/shell.h"
+#include "../include/state.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+int test_NOP(State *state, State *expected_state)
+{
+    // Load the instruction and set up the memory
+    state->memory[0] = NOP;
+
+    // Set up the expected register states
+    expected_state->pc = 1;
+
+    emulate8080(state);
+
+    return state_compare(state, expected_state);
+}
+
+int main(int argc, char *argv[])
+{
+    // Set up a states to test with
+    State *state = Init8080();
+    State *expected_state = Init8080();
+
+    int result;
+
+    switch (strtol(argv[1], NULL, 16))
+    {
+    case NOP: result = test_NOP(state, expected_state); break;
+    default: result = FAIL; // Test failed due to incorrect test parameter
+    }
+
+    // Clean up the state memory
+    Free8080(state);
+    Free8080(expected_state);
+
+    return result;
+}
