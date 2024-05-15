b59a927cfd0334144c59cdda5a0fdf289629c6a7
Craig Harris <70126287+cxhx441@users.noreply.github.com>
2024-05-15 09:46:09 -0700

    print video memory screen section to png.

---
+++ include/video.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "../include/state.h"
+
+#define VIDEO_MEMORY_START 0x2400
+#define VIDEO_MEMORY_END 0x3FFF // 0x2400 + 256*224/8 = 7168 (7K bytes)
+#define SCREEN_HEIGHT 256
+#define SCREEN_WIDTH 224
+
+void spinvaders_vram_matrix_to_png(State *state, int lbl_prefix);
---
+++ src/video.c
@@ -0,0 +1,40 @@
+#include "../include/video.h"
+#include "../include/disassemble8080p.h"
+#include "../include/libattopng.h"
+#include "../include/shell.h"
+#include "../include/state.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void spinvaders_vram_matrix_to_png(State *state, int lbl_prefix)
+{
+    libattopng_t *png = libattopng_new(256, 224, PNG_GRAYSCALE);
+
+    // the grid is sideways in memory, need to rotate CC for screen to be upright.
+    const int ROW_COUNT = 224;     // screen width
+    const int COL_COUNT = 256 / 8; // screen height
+
+    // uint8_t vram = state->memory[0x2400];
+    for (int r = 0; r < ROW_COUNT; r++)
+    {
+        for (int c = 0; c < COL_COUNT; c++)
+        {
+            uint16_t data_pos = (r * COL_COUNT) + c;
+            uint8_t  data     = state->memory[0x2400 + data_pos];
+            for (int bit_num = 0; bit_num < 8; bit_num++)
+            {
+                uint8_t data_bit = data & (0x01 << bit_num); // account for little endian
+                data_bit         = (data_bit == 0) ? 0 : 0xff;
+                libattopng_set_pixel(png, c * 8 + bit_num, r, data_bit);
+            }
+            // printf("%02x", state->memory[0x2400 + data_pos]);
+        }
+    }
+    char base_name[] = "test_space_inv.png";
+    char file_name[50];
+    sprintf(file_name, "../%d-%s", lbl_prefix, base_name);
+
+    libattopng_save(png, file_name);
+    libattopng_destroy(png);
+}
