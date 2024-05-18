30eae8032d3d4e8e8cf43ca6a0395c6581b7e9f8
J Web <weberjul@oregonstate.edu>
2024-05-18 16:06:38 -0700

    formatted switch/case braces correctly

--- src/controls.c
+++ src/controls.c
@@ -20,50 +20,76 @@ void emulate8080(State *state) {
 void update_input_state(State *state, SDL_Event *e) {
     // Handle key press events
     if (e->type == SDL_KEYDOWN) {
-        switch (e->key.keysym.sym) {
+        switch (e->key.keysym.sym)
+        {
         case SDLK_LEFT:
+        {
             state->input_state |= (1 << 5); // P1 joystick left
             break;
+        }
         case SDLK_RIGHT:
+        {
             state->input_state |= (1 << 6); // P1 joystick right
             break;
+        }
         case SDLK_SPACE:
+        {
             state->input_state |= (1 << 4); // P1 shoot button
             break;
+        }
         case SDLK_1:
+        {
             state->input_state |= (1 << 2); // P1 start button
             break;
+        }
         case SDLK_2:
+        {
             state->input_state |= (1 << 1); // P2 start button
             break;
+        }
         case SDLK_c:
+        {
             state->input_state &= ~(1 << 0); // Coin insert (active low)
             break;
+        }
         default:
             break;
         }
     }
     // Handle key release events
     else if (e->type == SDL_KEYUP) {
-        switch (e->key.keysym.sym) {
+        switch (e->key.keysym.sym)
+        {
         case SDLK_LEFT:
+        {
             state->input_state &= ~(1 << 5); // P1 joystick left
             break;
+        }
         case SDLK_RIGHT:
+        {
             state->input_state &= ~(1 << 6); // P1 joystick right
             break;
+        }
         case SDLK_SPACE:
+        {
             state->input_state &= ~(1 << 4); // P1 shoot button
             break;
+        }
         case SDLK_1:
+        {
             state->input_state &= ~(1 << 2); // P1 start button
             break;
+        }
         case SDLK_2:
+        {
             state->input_state &= ~(1 << 1); // P2 start button
             break;
+        }
         case SDLK_c:
+        {
             state->input_state |= (1 << 0); // Coin insert (inactive)
             break;
+        }
         default:
             break;
         }
