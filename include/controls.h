34d273b6aa93ca038b39c82d6204df40495f8cd5
J Web <weberjul@oregonstate.edu>
2024-05-21 15:03:59 -0700

    bc i didnt fix the header file last time

--- include/controls.h
+++ include/controls.h
@@ -1,21 +1,10 @@
-#ifndef CONTROLS_H
-#define CONTROLS_H
+#pragma once
 
 #include "SDL.h"
+#include "../include/state.h"
 #include <stdint.h>
 
-// Define the State structure
-typedef struct
-{
-    uint8_t a;
-    uint16_t pc;
-    uint8_t memory[0x10000];
-    uint8_t input_state;
-} State;
 
-// Function prototypes
-void init_emulator(State *state);
-void emulate8080(State *state);
-void update_input_state(State *state, SDL_Event *e);
+void get_keyboard_input(State *state, SDL_Event *e);
 
-#endif // CONTROLS_H
+
