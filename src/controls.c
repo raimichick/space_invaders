182db23e906566cd9b8e3983b83edfedd0935967
J Web <weberjul@oregonstate.edu>
2024-05-21 14:46:11 -0700

    added port 2, hardcoded ports

--- src/controls.c
+++ src/controls.c
@@ -1,25 +1,10 @@
 #include "../include/controls.h"
+#include "../include/state.h"
 #include "SDL.h"
 #include <stdio.h>
 
-// Function definitions
-void init_emulator(State *state)
-{
-    // Initialize all necessary parts of the state
-    state->a = 0;
-    state->pc = 0;
-    state->input_state = 0;
-    // Initialize other parts of the state as needed
-}
-
-// Emulate 8080 processor (stub function)
-void emulate8080(State *state)
-{
-    // Implement the 8080 emulation logic here
-}
-
 // Update input state based on SDL events
-void update_input_state(State *state, SDL_Event *e)
+void get_keyboard_input(State *state, SDL_Event *e)
 {
     // Handle key press events
     if (e->type == SDL_KEYDOWN)
@@ -28,32 +13,35 @@ void update_input_state(State *state, SDL_Event *e)
         {
         case SDLK_LEFT:
         {
-            state->input_state |= (1 << 5); // P1 joystick left
+            state->ports[1] |= 0b00100000; // P1 joystick left
+            state->ports[2] = state->ports[1]; // for player two
             break;
         }
         case SDLK_RIGHT:
         {
-            state->input_state |= (1 << 6); // P1 joystick right
+            state->ports[1] |= 0b01000000; // P1 joystick right
+            state->ports[2] = state->ports[1]; // for player two
             break;
         }
         case SDLK_SPACE:
         {
-            state->input_state |= (1 << 4); // P1 shoot button
+            state->ports[1] |= 0b00010000; // P1 shoot button
+            state->ports[2] = state->ports[1]; // for player two
             break;
         }
         case SDLK_1:
         {
-            state->input_state |= (1 << 2); // P1 start button
+            state->ports[1] |= 0b00000100; // P1 start button
             break;
         }
         case SDLK_2:
         {
-            state->input_state |= (1 << 1); // P2 start button
+            state->ports[1] |= 0b00000010; // P2 start button
             break;
         }
         case SDLK_c:
         {
-            state->input_state &= ~(1 << 0); // Coin insert (active low)
+            state->ports[1] |= 0b00000001; // Coin insert (active low)
             break;
         }
         default: break;
@@ -66,32 +54,35 @@ void update_input_state(State *state, SDL_Event *e)
         {
         case SDLK_LEFT:
         {
-            state->input_state &= ~(1 << 5); // P1 joystick left
+            state->ports[1] &= ~0b00100000; // P1 joystick left
+            state->ports[2] = state->ports[1]; // for player two
             break;
         }
         case SDLK_RIGHT:
         {
-            state->input_state &= ~(1 << 6); // P1 joystick right
+            state->ports[1] &= ~(1 << 6); // P1 joystick right
+            state->ports[2] = state->ports[1]; // for player two
             break;
         }
         case SDLK_SPACE:
         {
-            state->input_state &= ~(1 << 4); // P1 shoot button
+            state->ports[1] &= ~(1 << 4); // P1 shoot button
+            state->ports[2] = state->ports[1]; // for player two
             break;
         }
         case SDLK_1:
         {
-            state->input_state &= ~(1 << 2); // P1 start button
+            state->ports[1] &= ~(1 << 2); // P1 start button
             break;
         }
         case SDLK_2:
         {
-            state->input_state &= ~(1 << 1); // P2 start button
+            state->ports[1] &= ~(1 << 1); // P2 start button
             break;
         }
         case SDLK_c:
         {
-            state->input_state |= (1 << 0); // Coin insert (inactive)
+            state->ports[1] |= (1 << 0); // Coin insert (inactive)
             break;
         }
         default: break;
