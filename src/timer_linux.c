5beb40df4f2ba2ffd2dd66c773e7b14e4e0b9322
hermnich <122328256+hermnich@users.noreply.github.com>
2024-05-21 14:13:22 -0400

    Timer is working on linux machine

--- src/timer_linux.c
+++ src/timer_linux.c
@@ -6,49 +6,44 @@
 #include <time.h>
 #include <unistd.h>
 
-#ifdef _SC_MONOTONIC_CLOCK
-if (sysconf(_SC_MONOTONIC_CLOCK) > 0)
+uint64_t get_time_usecs()
 {
-    /* A monotonic clock is present */
-    uint64_t get_posix_clock_time_usecs()
+// Monotonic clock is available
+#ifdef _SC_MONOTONIC_CLOCK
+    if (sysconf(_SC_MONOTONIC_CLOCK) > 0)
     {
         struct timespec ts;
 
         if (clock_gettime(CLOCK_MONOTONIC, &ts) == 0)
-            return (uint64_t)(ts.tv_sec * 1000000 + ts.tv_nsec / 1000);
-        else
-            return 0;
-    }
-}
-else // monotonic clock is not present
-{
-    uint64_t get_gtod_clock_time_usecs()
-    {
-        struct timeval tv;
+            return ts.tv_sec * 1000000 + ts.tv_nsec / 1000;
 
-        if (gettimeofday(&tv, NULL) == 0)
-            return (uint64_t)(tv.tv_sec * 1000000 + tv.tv_usec);
-        else
-            return 0;
+        return 0;
     }
-}
 #endif
+
+    // Monotonic clock is not available
+    struct timeval tv;
+
+    if (gettimeofday(&tv, NULL) == 0) return tv.tv_sec * 1000000 + tv.tv_usec;
+
+    return 0;
+}
 
 uint64_t cur_time_val = 0;
 uint64_t prev_time_val = 0;
 uint64_t elapsed_usecs = 0;
 
-void init_timer() { prev_time_val = get_posix_clock_time_usec(); }
+void init_timer() { prev_time_val = get_time_usecs(); }
 
 void wait_for_frametime_elapsed(float frametime_us)
 {
-    cur_time_val = get_posix_clock_time_usec();
+    cur_time_val = get_time_usecs();
     elapsed_usecs = cur_time_val - prev_time_val;
 
     // Keep checking until you pass the frametime.
     while (elapsed_usecs < frametime_us)
     {
-        cur_time_val = get_posix_clock_time_usec();
+        cur_time_val = get_time_usecs();
         elapsed_usecs = cur_time_val - prev_time_val;
     }
 
