b2bc5649d631238258b0ef844ff34d02eb2f4c63
Craig Harris <70126287+cxhx441@users.noreply.github.com>
2024-05-27 07:53:53 -0700

    fix num lives editor on 2 lsb of port 2 in controls.c

--- src/controls.c
+++ src/controls.c
@@ -16,14 +16,12 @@ void update_keyboard_input(State *state, SDL_Event *e)
         case SDLK_LEFT:  { state->ports[1] |=  (1 << 5); state->ports[2] = state->ports[1]; break; } // P1 & P2 joystick left
         case SDLK_RIGHT: { state->ports[1] |=  (1 << 6); state->ports[2] = state->ports[1]; break; } // P1 & P2 joystick right
 
-
-        case SDLK_l:     { state->ports[2] |=  ((state->ports[2] + 1) % 2); break; } // tilt 'button'
+        case SDLK_l:     { state->ports[2] |=  ((state->ports[2] + 1) % 4); break; } // # of lives (0:3,1:4,2:5,3:6)
         case SDLK_t:     { state->ports[2] |=  (1 << 2); break; } // tilt 'button'
         case SDLK_b:     { state->ports[2] |=  (1 << 3); break; } // dipswitch bonus life at 1:1000,0:1500
         case SDLK_i:     { state->ports[2] |=  (1 << 7); break; } // dipswitch coin info 1:off,0:on
         case SDLK_q:     { state->halt = 1; break;}
         default: break;
-            // BIT 0,1 dipswitch number of lives (0:3,1:4,2:5,3:6)
         }
     }
     // Handle key release events
