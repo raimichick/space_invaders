5e729065e33a0d168d58f67508c170b6ad8e8d16
Craig H <70126287+cxhx441@users.noreply.github.com>
2024-06-04 16:54:22 -0700

    add A, D, F for left right fire

--- src/controls.c
+++ src/controls.c
@@ -11,9 +11,12 @@ void update_keyboard_input(State *state, SDL_Event *e)
         case SDLK_c:     { state->ports[1] &= ~(1 << 0); break; } // Coin insert (active low)
         case SDLK_2:     { state->ports[1] |=  (1 << 1); break; } // P2 start button
         case SDLK_1:     { state->ports[1] |=  (1 << 2); break; } // P1 start button
+        case SDLK_f:
         case SDLK_UP:
         case SDLK_SPACE: { state->ports[1] |=  (1 << 4); state->ports[2] = state->ports[1]; break; } // P1 & P2 shoot button
+        case SDLK_a:
         case SDLK_LEFT:  { state->ports[1] |=  (1 << 5); state->ports[2] = state->ports[1]; break; } // P1 & P2 joystick left
+        case SDLK_d:
         case SDLK_RIGHT: { state->ports[1] |=  (1 << 6); state->ports[2] = state->ports[1]; break; } // P1 & P2 joystick right
 
         case SDLK_l:     { state->ports[2] |=  ((state->ports[2] + 1) % 4); break; } // # of lives (0:3,1:4,2:5,3:6)
@@ -32,9 +35,12 @@ void update_keyboard_input(State *state, SDL_Event *e)
         case SDLK_c:     { state->ports[1] |=  (1 << 0); break; } // Coin insert (inactive)
         case SDLK_2:     { state->ports[1] &= ~(1 << 1); break; } // P2 start button
         case SDLK_1:     { state->ports[1] &= ~(1 << 2); break; } // P1 start button
+        case SDLK_f:
         case SDLK_UP:
         case SDLK_SPACE: { state->ports[1] &= ~(1 << 4); state->ports[2] = state->ports[1]; break; } // P1 & P2 shoot button
+        case SDLK_a:
         case SDLK_LEFT:  { state->ports[1] &= ~(1 << 5); state->ports[2] = state->ports[1]; break; } // P1 & P2 joystick left
+        case SDLK_d:
         case SDLK_RIGHT: { state->ports[1] &= ~(1 << 6); state->ports[2] = state->ports[1]; break; } // P1 & P2 joystick right
 
         case SDLK_t:     { state->ports[2] &=  (1 << 2); break; } // tilt 'button'
